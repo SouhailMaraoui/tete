2b3d93f27bb0c9f34e013bcc4ef422128cdcf703
Souhail Maraoui <souhail.maraoui@usherbrooke.ca>
2021-07-04 06:52:37 -0700

    Added YAML config file interpreter to 'fileManager'

--- BuildUs/fileManager.cpp
+++ BuildUs/fileManager.cpp
@@ -75,3 +75,89 @@ std::string writeInFile(const std::string& filename, const std::string& content,
     file.close();
     return filename;
 }
+
+/*
+A function that take the yaml config file and build an Object of type BuildYAML.
+*/
+BuildYAML buildYAMLObject(std::string filename)
+{
+    BuildYAML buildYAML;
+
+    std::string content=readFile(filename); 
+    std::istringstream stream(content);
+    
+
+    std::string line,parent,subparent;
+
+    while(std::getline(stream,line))
+    {
+        if(line.rfind("project",0)==0){
+            parent="project";
+        }
+        else if(line.rfind("deps_include",0)==0){
+            parent="deps_include";
+            continue;
+        }
+        else if(line.rfind("deps_library",0)==0){
+            parent="deps_library";
+            continue;
+        }
+        else if(line.rfind("compile",0)==0){
+            parent="compile";
+            continue;
+        }
+        else if(line.rfind("files",0)==0){
+            parent="files";
+            continue;
+        }
+
+        std::istringstream iss(line);
+        std::string a, b,c,d;
+        iss>>a>>b>>c>>d;
+
+        
+        /*
+        In case there was a space between an element and the colon ":", remove that space and translate to the left.
+        So for example "project : app2" will turn into "project: app2"
+        Or " - f1 : file1.cpp" will turn into "- f1: file1.cpp"
+        */
+        if(b==":")
+        {
+            a+=b;
+            b=c;
+        }
+        else if(c==":")
+        {
+            b+=c;
+            c=d;
+        }
+
+        if(a=="libs:"){
+            subparent="libs";
+            continue;
+        }
+
+        if(parent=="project"){
+            buildYAML.setProject(b);
+        }
+        else if(parent=="deps_include"){
+            if(a=="var:") {
+                buildYAML.setDeps_include_var(b);
+            }
+        }
+        else if(parent=="deps_library"){
+            if(a=="var:") {
+                buildYAML.setDeps_library_var(b);
+            }
+            if(subparent=="libs") 
+                buildYAML.pushDeps_library_lib(b);
+        }
+        else if(parent=="compile"){
+            buildYAML.pushCompile(b.substr(0,b.size()-1),c);
+        }
+        else if(parent=="files"){
+            buildYAML.pushFiles(b);
+        }
+    }
+    return buildYAML;
+}
