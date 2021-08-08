be846b0b04023472e450dd590ac2ad4cb3ee75a5
Souhail Maraoui <souhail.maraoui@usherbrooke.ca>
2021-08-08 02:32:26 -0400

    Added option to exec a cmd in WorkUs from DeployUs

---
+++ DeployUS/Controllers/ExecController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeployUS.Controllers
+{
+    [Route("exec")]
+    [ApiController]
+    public class InterfaceController : ControllerBase
+    {
+
+        [HttpPost]
+        public string Run(Worker worker, Script script)
+        {
+            using (var client = new HttpClient())
+            {
+                var toSend = new StringContent(JsonConvert.SerializeObject(script), Encoding.UTF8, "application/json");
+                return client.PostAsync(worker.address + "exec", toSend).Result;
+            }
+        }
+    }
+}
